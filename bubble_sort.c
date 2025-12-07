d898d97e22022ba0bbf1122e0ed7edfe3782eb66
HarshitaSnehal <h.snehal24@gmail.com>
2025-12-07 17:18:07 +0530

    Added bubble sort code

---
+++ bubble_sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+int arr[50];
+int i, j, n, temp;
+printf("Enter number of elements: ");
+scanf("%d",&n);
+
+printf("Enter %d integers:\n", n);
+for(i=0; i<n; i++)
+{
+scanf("%d",&arr[i]);
+}
+
+//bubble sort
+for(i=0; i<n-1; i++)
+{
+for(j=0; j<n-1-i; j++)
+{
+if(arr[j]>arr[j+1])
+{
+temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+printf("Sorted array:\n");
+for(i=0; i<n; i++)
+{
+printf("%d ",arr[i]);
+}
+return(0);
+}
+
